cbd7018b25a7567e9b33700acc8b59db8d93c4d0
Diegod96 <ddphillyfan@gmail.com>
2020-08-03 22:11:36 -0400

    Upload Component created & bootstrap

---
+++ app/src/Upload.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Upload extends Component {
+    state = { }
+    render() {
+        return(
+            <div>
+                Hello World
+            </div>
+        );
+    }
+}
+
+export default Upload;
