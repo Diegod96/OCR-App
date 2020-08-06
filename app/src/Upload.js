2fc8cd5521401130dec4d6342545be463f78ad84
Diegod96 <ddphillyfan@gmail.com>
2020-08-06 19:43:15 -0400

    Handle Submit & Form created

--- app/src/Upload.js
+++ app/src/Upload.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
+import FileBase64 from "react-file-base64";
+import {Button, Form, FormGroup, Label, FormText} from "reactstrap";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faInfoCircle} from '@fortawesome/free-solid-svg-icons';
 
 class Upload extends Component {
-    state = { }
+    state = {
+        confirmation : ""
+    }
+
+    async handleSubmit(event) {
+        event.preventDefault();
+        this.setState({confirmation: "Uploading..."});
+    }
+
+
     render() {
+        const processing="Processing document...";
         return(
-            <div>
-                Hello World
+            <div className="row">
+                <div className="col-6 offset-3">
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <h3 className="text-danger">{processing}</h3>
+                            <h6>Upload Your Invoice</h6>
+                            <FormText color="muted">PNG or JPG</FormText>
+                        </FormGroup>
+                    </Form>
+                </div>
             </div>
         );
     }
